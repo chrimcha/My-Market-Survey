678578297f7eb3a7ccb963a43c12a518b4c63a4a
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-10-13 14:00:14 -0500

    set up vite app, deleted some default content, set up index page

---
+++ My[Market]Survey_UI/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My[Market]Survey</h1>
+      
+      <p className="app-description">
+      Collect customer feedback from products offered at your market, pop-up, booth, or shop. Allows customers to select predetermined prompts or create their own, then organizes feedback data into charts and lists per product, event, or all together.
+      </p>
+
+      <button>Create a Survey</button>
+    </>
+  )
+}
+
+export default App
